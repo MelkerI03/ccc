ccf187262e5283f5bc0e0594d03057d036ef1a29
Melker Isaksson <Melker.Isaksson@outlook.com>
2025-05-10 16:36:22 +0200

    More initialisation

--- flake.nix
+++ flake.nix
@@ -1,56 +1,40 @@
 {
-  description = "ccc is a lightweight C compiler written in haskell";
+  description = "A Haskell-based C compiler called ccc";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  
-  outputs = inputs:
-    let
-      overlay = final: prev: {
-        haskell = prev.haskell // {
-          packageOverrides = hfinal: hprev:
-            prev.haskell.packageOverrides hfinal hprev // {
-              TheNameOfMyExecutable = hfinal.callCabal2nix "ccc" ./. { };
-            };
-          };
-               OnCallComp = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.ccc;
-  };
-  perSystem = system:
-    let
-      pkgs = import inputs.nixpkgs { inherit system; overlays = [ overlay ]; config = { allowUnfree = true; };};
-    in
-    {
-       devShell = pkgs.haskellPackages.shellFor {
-         withHoogle = true;
-         packages = p: [ p.OnCallComp];
-  
-         nativeBuildInputs = with pkgs.haskellPackages; [
-           cabal-install
-           haskell-language-server
-           hlint
-           ormolu
-           ghcid
- #             pkgs.bashInteractive
- #             pkgs.zlib
- #             (pkgs.vscode-with-extensions.override {
- #             vscode = pkgs.vscodium;
- #             vscodeExtensions = with pkgs.vscode-extensions; [
- #                  asvetliakov.vscode-neovim
- #                  dracula-theme.theme-dracula
- #                  haskell.haskell
- #                  jnoortheen.nix-ide
- #                  justusadam.language-haskell
- #                  mkhl.direnv
- #              ];  
- #             }
-         ]; 
-   };
-   defaultPackage = pkgs.ccc;
- };
- in
-   { inherit overlay; } // inputs.flake-utils.lib.eachSystem [ "x86_64-linux" ] perSystem;
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        haskellPkgs = pkgs.haskellPackages;
+        devTools = [
+          haskellPkgs.cabal-install
+          haskellPkgs.haskell-language-server
+          haskellPkgs.hlint
+          haskellPkgs.ormolu
+          haskellPkgs.ghcid
+        ];
+      in
+      {
+        packages.default = haskellPkgs.callCabal2nix "ccc" ./. {};
+
+        devShells.default = haskellPkgs.shellFor {
+          packages = _: [ self.packages.${system}.default ];
+          buildInputs = devTools ++ [ pkgs.nixpkgs-fmt ];
+          shellHook = ''
+            echo "Welcome to the ccc development environment!"
+            echo "Available tools: cabal, haskell-language-server, hlint, ormolu, ghcid"
+          '';
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/ccc";
+        };
+      }
+    );
 }
--- src/lexer.hs
+++
@@ -1,7 +0,0 @@
-module Lexer where
-
-findAll :: Int -> Int -> Double
-findAll _ _ = 1
-
-findSome :: Int -> Int
-findSome a = map 
